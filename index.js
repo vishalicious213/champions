862fb0d88d38db6267f938ed6de6565e4282f99f
vishalicious213 <vishalicious213@gmail.com>
2023-05-16 09:30:08 -0700

    renamed ID for endorsement to endorsement-input. added new class for endorsement and styled it (for individual endorsement comments)

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const button = document.getElementById("button")
-const endorsementInput = document.getElementById("endorsement")
+const endorsementInput = document.getElementById("endorsement-input")
 const endorsementsList = document.getElementById("endorsements-list")
 
 // ⬇️ EVENT LISTENERS ⬇️
+
 button.addEventListener("click", function() {
     publishEndorsement()
 })
 
 
 // ⬇️ EVENT HANDLERS ⬇️
+
 function publishEndorsement() {
     console.log("clicked")
 }
 
 
 // ⬇️ RENDER THE APP ⬇️
+
+function renderEndorsements() {
+    endorsementsList.innerHTML = `
+        <div class="endorsement">Hi Bob! Your React Router course is so good. The students are going to LOVE IT. I’m so excited for the launch :) 🔥 Per</div>
+    `
+}
+
+renderEndorsements()
