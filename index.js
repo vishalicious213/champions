a663c3e4fc034061bef759b348d7479da30cc74e
vishalicious213 <vishalicious213@gmail.com>
2023-05-16 09:11:47 -0700

    added event listener for button

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const button = document.getElementById("button")
 const endorsementInput = document.getElementById("endorsement")
 const endorsementsList = document.getElementById("endorsements-list")
+
+// ⬇️ EVENT LISTENERS ⬇️
+button.addEventListener("click", function() {
+    console.log("clicked")
+})
+
+
+// ⬇️ EVENT HANDLERS ⬇️
+
+
+
+// ⬇️ RENDER THE APP ⬇️
