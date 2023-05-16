7e85b401c29e09960fe8897f7516d612707624e7
vishalicious213 <vishalicious213@gmail.com>
2023-05-16 09:35:20 -0700

    added appSettings for firebase db

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+// firebase settings
+const appSettings = {
+    databaseURL: "https://champions-4e9b8-default-rtdb.firebaseio.com/"
+}
+
 // app elements
 const button = document.getElementById("button")
 const endorsementInput = document.getElementById("endorsement-input")
